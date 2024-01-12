3b66c4a842ecc1e9fe90329705a595e9454955b0
Nadern96 <nadernabilmansour@gmail.com>
2024-01-12 04:42:14 +0200

    added reviews_list template

--- reviews/views.py
+++ reviews/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from .forms import CompanyReviewForm
 from django.contrib import messages #import messages
 from django.utils.translation import gettext as _
-
+from .models import CompanyReview
 
 # Create your views here.
-from .models import CompanyReview
-def index(request):
+
+def review(request):
     if request.method == 'POST':
         form = CompanyReviewForm(request.POST)
         if form.is_valid():
@@ -19,3 +19,10 @@ def index(request):
         'form': form
     }
     return render(request, 'review_form.html', context=context)
+
+def list_reviews(request):
+    reviews = CompanyReview.objects.all()
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'reviews_list.html', context=context)
