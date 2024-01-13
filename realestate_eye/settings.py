3571be4e0eac418f2253310b6b329a214c15f994
Nadern96 <nadernabilmansour@gmail.com>
2024-01-14 01:06:18 +0200

    added elephantSql connection string

--- realestate_eye/settings.py
+++ realestate_eye/settings.py
@@ -89,13 +89,22 @@ WSGI_APPLICATION = "realestate_eye.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fikumbgk',
+        'USER': 'fikumbgk',
+        'PASSWORD': 'qqQxai-rcwey7LDj35hp6qrIgeZKBQH1',
+        'HOST': 'postgres://fikumbgk:qqQxai-rcwey7LDj35hp6qrIgeZKBQH1@heffalump.db.elephantsql.com/fikumbgk',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
